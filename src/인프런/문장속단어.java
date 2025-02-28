b656973271f5ed3962ac118886a3d4379c3b2bc5
nam3280 <nam3280@gmail.com>
2025-02-28 18:27:11 +0900

    2025/02/28

---
+++ src/인프런/문장속단어.java
@@ -0,0 +1,30 @@
+package 인프런;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 문장속단어 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+
+        StringTokenizer token = new StringTokenizer(input);
+
+        int max = 0;
+        char[] answer = new char[0]; 
+        while(token.hasMoreTokens()){
+            char[] tokenInput = token.nextToken().toCharArray();
+            
+            if(max < tokenInput.length) {
+                max = tokenInput.length;
+                answer = tokenInput;
+            }
+        }
+
+        for (char c : answer)
+            System.out.print(c);
+    }
+}
