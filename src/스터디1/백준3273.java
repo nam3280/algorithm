9a65f0f573b757d70b6fe72ee1917796fe4ef678
nam3280 <nam3280@gmail.com>
2025-03-14 17:42:36 +0900

    2025/03/14

---
+++ src/스터디1/백준3273.java
@@ -0,0 +1,37 @@
+package 스터디1;
+
+import java.io.*;
+import java.util.*;
+
+public class 백준3273 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[num];
+
+        StringTokenizer token = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = Integer.parseInt(token.nextToken());
+
+        int x = Integer.parseInt(br.readLine());
+
+        HashSet<Integer> set = new HashSet<>();
+
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int cal = x - arr[i];
+
+            if (set.contains(cal)) {
+                count++;
+                set.remove(cal);
+            }
+            else
+                set.add(arr[i]);
+        }
+
+        System.out.println(count);
+    }
+}
+
