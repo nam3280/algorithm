3303f36ce5266ee8a7cc40c0b27dcd239bb2e03d
nam3280 <nam3280@gmail.com>
2025-03-14 18:28:42 +0900

    2025/03/14

---
+++ src/인프런/중복문자제거.java
@@ -0,0 +1,19 @@
+package 인프런;
+
+import java.io.*;
+
+public class 중복문자제거 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (sb.toString().indexOf(str.charAt(i)) == -1)
+                sb.append(str.charAt(i));
+        }
+
+        System.out.println(sb);
+    }
+}
