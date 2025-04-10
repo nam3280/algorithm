12eb7ac9e6717a250abf36bab3d3d6a43afd812b
nam3280 <nam3280@naver.com>
2025-04-10 14:24:49 +0900

    2025/04/10

---
+++ src/인프런/유효한팰린드롬.java
@@ -0,0 +1,20 @@
+package 인프런;
+
+import java.io.*;
+
+public class 유효한팰린드롬 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] arr = br.readLine().toUpperCase().replaceAll("[^A-Z]","").toCharArray();
+
+        boolean same = true;
+        for (int i = 0; i < arr.length / 2; i++) {
+            if(arr[i] != arr[arr.length - 1 - i]){
+                same = false;
+                break;
+            }
+        }
+
+        System.out.println(same ? "YES" : "NO");
+    }
+}
---
+++ src/인프런/회문문자열.java
@@ -0,0 +1,20 @@
+package 인프런;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 회문문자열 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] arr = br.readLine().toUpperCase().toCharArray();
+
+        boolean same = true;
+        for (int i = 0; i < arr.length / 2; i++) {
+            if(arr[i] != arr[arr.length - 1 - i])
+                same = false;
+        }
+
+        System.out.println(same ? "YES" : "NO");
+    }
+}
