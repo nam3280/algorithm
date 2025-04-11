f6833df82de9ba2dffe086cfb6bde6b1095bb5f5
nam3280 <nam3280@naver.com>
2025-04-11 22:03:20 +0900

    2025/04/11

---
+++ src/인프런/숫자만추출.java
@@ -0,0 +1,19 @@
+package 인프런;
+
+import java.io.*;
+
+public class 숫자만추출 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] arr = br.readLine().toUpperCase().replaceAll("[A-Z]", "").toCharArray();
+
+        StringBuilder num = new StringBuilder();
+        for (char c : arr)
+            num.append(c);
+
+        int answer = Integer.parseInt(num.toString());
+
+        System.out.println(answer);
+    }
+}
