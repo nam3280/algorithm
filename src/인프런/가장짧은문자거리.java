23c33ea58dda2691d0af9d77cf91115e9351c3ce
nam3280 <nam3280@naver.com>
2025-04-12 14:39:41 +0900

    2025/04/12

---
+++ src/인프런/가장짧은문자거리.java
@@ -0,0 +1,38 @@
+package 인프런;
+
+import java.io.*;
+
+public class 가장짧은문자거리 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] st = br.readLine().split(" ");
+
+        char[] arr = st[0].toCharArray();
+
+        int[] answer = new int[arr.length];
+
+        int count = 1000;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] != st[1].charAt(0))
+                answer[i] = ++count;
+            else {
+                count = 0;
+                answer[i] = count;
+            }
+        }
+
+        count = 1000;
+        for (int i = answer.length - 1; i >= 0; i--) {
+            if(arr[i] != st[1].charAt(0))
+                count++;
+            else
+                count = 0;
+
+            answer[i] = Math.min(answer[i], count);
+        }
+
+        for (int i : answer)
+            System.out.print(i + " ");
+    }
+}
