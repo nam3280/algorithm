fbd1ab556688636c5fb6c70ca4afa8dae5f60f3f
nam3280 <nam3280@naver.com>
2025-04-13 15:55:36 +0900

    2025/04/13

---
+++ src/인프런/문자열압축.java
@@ -0,0 +1,32 @@
+package 인프런;
+
+import java.io.*;
+
+public class 문자열압축 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] arr = br.readLine().split("");
+
+        StringBuilder sb = new StringBuilder(arr[0]);
+
+        int count = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if(sb.substring(sb.length() - 1).equals(arr[i])) {
+                count++;
+
+                if(i == arr.length - 1)
+                    sb.append(count);
+            }
+            else if(count == 1)
+                sb.append(arr[i]);
+            else {
+                sb.append(count);
+                sb.append(arr[i]);
+                count = 1;
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
