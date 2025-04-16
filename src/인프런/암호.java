3c52dc653846b988c42aa1d43d40cec4d80c1d5f
nam3280 <nam3280@naver.com>
2025-04-16 15:29:35 +0900

    2025/04/16

---
+++ src/인프런/암호.java
@@ -0,0 +1,25 @@
+package 인프런;
+
+import java.io.*;
+
+public class 암호 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int input1 = Integer.parseInt(br.readLine());
+        String input2 = br.readLine();
+
+        for (int i = 0; i < input1; i++) {
+            //0부터 6까지 가져온다.
+            String temp = input2.substring(0,7).replace('#', '1').replace('*', '0');
+
+            //2진수로 계산
+            int num = Integer.parseInt(temp,2);
+
+            System.out.print((char)num);
+
+            //0부터 6까지 문자열을 제외한 나머지 문자열을 input2에 담는다.
+            input2 = input2.substring(7);
+        }
+    }
+}
