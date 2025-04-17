d36c38e801c486c0a60001f6d5c65df8949e2afa
nam3280 <nam3280@naver.com>
2025-04-17 15:37:53 +0900

    2025/04/17

---
+++ src/인프런/큰수출력하기.java
@@ -0,0 +1,22 @@
+package 인프런;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 큰수출력하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        String[] arr = br.readLine().split(" ");
+
+        System.out.print(arr[0] + " ");
+
+        for (int i = 1; i < arr.length; i++) {
+            if(Integer.parseInt(arr[i - 1]) < Integer.parseInt(arr[i]))
+                System.out.print(arr[i] + " ");
+        }
+    }
+}
